3095c1fb9867fb61a8f0dc797edced732ec813c5
Raiyanbhai <101969805+Raiyanbhai@users.noreply.github.com>
2022-03-24 19:49:09 +0545

    Add files via upload

---
+++ friend fun use in member of class.cpp
@@ -0,0 +1,27 @@
+﻿//Find the area of circle using friend function in member of class
+#include <iostream>
+using namespace std;
+class Circle
+{
+	private: 
+           float r;
+	public:
+	  	   void getdata()
+		   {
+		   cout<<"Enter any radius";
+		   cin>>r;
+		   }
+	friend void area (Circle c);
+};
+void area(Circle c)
+{
+float;
+cout<<"your radius is :"<<(3.14*c.r*c.r)<<endl;
+}
+int main()
+{
+Circle c;
+c.getdata();
+area(c);
+return 0;
+}
